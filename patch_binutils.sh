5a860ca7b11bdf088c4e4dbc01224a2152ad6c8a
Xavier Del Campo <xavi.dcr@tutanota.com>
2019-10-30 12:16:12 +0100

    Fixed typo and improved .tar.xz support
    Thanks again to pacman from the ST community for the tip

--- patch_binutils.sh
+++ patch_binutils.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-which wget 2>/dev/null 1&>&2 && dl="wget -nc" || dl="curl -OLk --retry 5"
+which wget 2>/dev/null 1>&2 && dl="wget -nc" || dl="curl -OLk --retry 5"
 $dl ftp.gnu.org/gnu/binutils/binutils-2.30.tar.xz
 $dl ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz
 mkdir -p binutils-2.30
-tar -xvf gdb-8.1.tar.xz --strip-components=1 --directory=binutils-2.30
-tar -xvf binutils-2.30.tar.xz
+tar -xJvf gdb-8.1.tar.xz --strip-components=1 --directory=binutils-2.30
+tar -xJvf binutils-2.30.tar.xz
 for f in ./binutils_patches/*.patch
 do
 patch -N -p 1 -d binutils-2.30 <$f
